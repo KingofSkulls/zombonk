80c5b6590c940226b82fc12350b59510a574bb3e
Ps20ndean <61612209+KingofSkulls@users.noreply.github.com>
2022-04-02 19:25:05 -0400

    flashlight dims

--- Scenes/battery.gd
+++ Scenes/battery.gd
@@ -1,6 +1,8 @@
 extends Area
 
-
+var verticaldrift =0
+var driftspeed=0.0005
+var vel := Vector3()
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -11,7 +13,16 @@ func _ready():
 	pass # Replace with function body.
 
 func _physics_process(delta):
+	vel.x =0
+	vel.y=0
+	vel.z=0
 	rotate_y(delta*2) #rotates in degrees
+	verticaldrift+=driftspeed
+	vel.y+=driftspeed
+	transform.origin+=vel
+	if abs(verticaldrift) > .02:
+		driftspeed*=-1
+		
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
