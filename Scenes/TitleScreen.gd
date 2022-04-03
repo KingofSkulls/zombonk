c917e3e878029e61c84684fec0f22f34d27d544e
Elijah <aberjack776@gmail.com>
2022-04-02 21:32:27 -0400

    Title Screen presentable
    The title screen is now MVP. If there is time, I will return to it. It does not work in fullscreen, which is the next biggest priority

--- Scenes/TitleScreen.gd
+++ Scenes/TitleScreen.gd
@@ -8,12 +8,12 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
+func _process(delta: float) -> void:
+	title_slide_in(delta)
 
 
 func _on_Start_pressed() -> void:
@@ -25,3 +25,23 @@ func _on_Credits_pressed() -> void:
 
 func _on_Exit_pressed() -> void:
 	get_tree().quit()
+
+
+
+func _on_Start_mouse_entered() -> void:
+	get_node("Hand").rect_position.y = 382
+
+
+func _on_Credits_mouse_entered() -> void:
+	get_node("Hand").rect_position.y = 437
+	
+func _on_Exit_mouse_entered() -> void:
+	get_node("Hand").rect_position.y = 494
+
+
+
+func title_slide_in(delta: float) ->void:
+	var  slideSpeed: float = 400.0
+	if(get_node("TitleText").rect_position.y < -220):
+		get_node("TitleText").rect_position.y += slideSpeed * delta
+
